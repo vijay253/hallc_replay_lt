031c3b5037912347806756de738fa77a150ab5ca
Vijay Kumar <vijay@lemming.phys.uregina.ca>
2020-07-10 10:25:14 -0600

    calibration plots updated

--- plot_code/macro2.C
+++ plot_code/macro2.C
@@ -1,26 +1,51 @@
-// Reads the points from a file and produces a simple graph.
+// Code for plotting the data
+// Author... VIJAY KUMAR..... 
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+#include <sstream>
+#include <vector>
+#include "TGraph.h"
+#include "TMultiGraph.h"
+#include "TGraphErrors.h"
+#include "TGaxis.h"
+#include "TAxis.h"
+#include "TCanvas.h"
 
-int macro2()
+int calib()
+
 {
-  auto c = new TCanvas();c->SetGrid();
+  gROOT->ForceStyle();
 
-  TGraphErrors graph_expected("input.txt", "%lg %lg %lg");
-                               
-  graph_expected.SetTitle( "Consistency of calibration constants for PMT1;" "Run Number;" " Calibration constant");			  			 			 
-  graph_expected.SetFillColor(kYellow);
-  graph_expected.DrawClone("E3AL"); // E3 draws the band
+  TCanvas * plot = new TCanvas("plot", "Consistency of parameters"); plot->SetGrid();
+  TMultiGraph* mg = new TMultiGraph(); mg->SetTitle("PMT 1");  // Add title of the plot
 
-  TGraphErrors graph("input.txt","%lg %lg %lg");
-  graph.SetMarkerStyle(kCircle);
-  graph.SetFillColor(0);
-  graph.DrawClone("PESame");
+  TGraphErrors *gr1 = new TGraphErrors("input_pmt4_guess1.txt","%lg %lg %lg");
+  gr1->SetMarkerColor(kRed);
+  gr1->SetMarkerStyle(kFullCircle);
+  gr1->SetLineColor(kBlue);
 
-  // Draw the Legend
-  TLegend leg(.1,.7,.3,.9,"Lab. Lesson 2");
-  leg.SetFillColor(0);
-  leg.AddEntry(&graph_expected,"First Guess");
-  leg.AddEntry(&graph,"Second Guess");
-  leg.DrawClone("Same");
-  graph.Print();
-  return 0;
+  TGraphErrors *gr2 = new  TGraphErrors("input_pmt4_guess2.txt","%lg %lg %lg");
+  gr2->SetMarkerColor(kBlue);    
+  gr2->SetMarkerStyle(kFullSquare);
+  gr2->SetLineColor(kRed);
+
+  mg->Add(gr1);
+  mg->Add(gr2);
+  mg->Draw("apl");
+  mg->GetXaxis()->SetTitle("Run Numbers");                // Add title of the X axis
+  mg->GetYaxis()->SetTitle("Calibration Constants");     // Add title of the Y axis
+  mg->GetHistogram()->SetMaximum(8);                    // Set the range of Y axis           
+  mg->GetHistogram()->SetMinimum(5.2);
+
+  TLegend* leg = new TLegend(.6,.7,.9,.9);            // Draw the Legend
+  leg->SetFillColor(0);
+  leg->AddEntry(&*gr1,"First Guess");
+  leg->AddEntry(&*gr2,"Second Guess");
+  leg->Draw("Same");                 
+  plot->SaveAs("pmt4.png");                        // Save the plot
+  return 0;                                       // Finish the function
+
+
+
 }
